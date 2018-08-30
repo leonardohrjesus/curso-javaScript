1f6e37d42415b339eff5b397b82161f9acd944cc
Leonardo <leonardo.hrjesus@hotmail.com>
2018-08-29 23:12:12 -0300

    penultimo captulo

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -6,14 +6,11 @@ class DateHelper {
     }
     
     static dataParaTexto(data) {
-//        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
-        return "25/05/2018";
-
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
     
     static textoParaData(texto) {
         
-
         if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
             throw new Error('Deve estar no formato aaaa-mm-dd');
              
